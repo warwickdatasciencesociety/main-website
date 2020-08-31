b66b52b54f346854105256a48c4f1f30928f34be
Bhavik Makwana <bhavikmakwana09@gmail.com>
2020-08-31 15:08:58 +0100

    Remove padding

--- js/carousel.js
+++ js/carousel.js
@@ -1,43 +1,13 @@
 $(document).ready(function() {
  
 
-  // $('.slick-center').slick({
-  //   centerMode: true,
-  //   centerPadding: '60px',
-  //   slidesToShow: 3,
-  //   autoplay: true,
-  //   autoplaySpeed: 3000,
-  //   arrows: true,
-  //   focusOnSelect: false,
-  //   responsive: [
-  //     {
-  //       breakpoint: 768,
-  //       settings: {
-  //         arrows: false,
-  //         centerMode: true,
-  //         centerPadding: '60px',
-  //         focusOnSelect: false,
-  //         slidesToShow: 3
-  //       }
-  //     },
-  //     {
-  //       breakpoint: 480,
-  //       settings: {
-  //         arrows: false,
-  //         centerMode: true,
-  //         centerPadding: '60px',
-  //         focusOnSelect: false,
-  //         slidesToShow: 1
-  //       }
-  //     }
-  //   ]
-  // });
   
   new Glide('.glide', {
     type: 'carousel',
     startAt: 0,
     perView: 3,
-    autoplay: 3000,
+    autoplay: 2000,
+    dragThreshold: 0,
     breakpoints: {
       1023: {
         perView: 1,
