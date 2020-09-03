7d04821da5b03af619e3f9e2de7dc4e76132bb2c
Bhavik Makwana <bhavikmakwana09@gmail.com>
2020-09-03 21:23:54 +0100

    change font colour and other small fixes

--- js/app.js
+++ js/app.js
@@ -85,7 +85,7 @@ particlesJS('corevalues',
       "detect_on": "canvas",
       "events": {
         "onhover": {
-          "enable": true,
+          "enable": false,
           "mode": "repulse"
         },
         "onclick": {
